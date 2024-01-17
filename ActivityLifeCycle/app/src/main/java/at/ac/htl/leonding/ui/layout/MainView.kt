3a7cfc04ccb4722d815d668540f54b2c7f547a4f
Paul Krenmayr <paul.krenmayr@gmail.com>
2024-01-17 20:57:26 +0100

    Using Lifecycle Functions from android Studio

---
+++ ActivityLifeCycle/app/src/main/java/at/ac/htl/leonding/ui/layout/MainView.kt
@@ -0,0 +1,46 @@
+package at.ac.htl.ui.layout
+
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import at.ac.htl.leonding.ui.theme.ActivityLifeCycleTheme
+
+class MainView {
+
+}
+fun setContentView(activity: ComponentActivity) {
+    activity.setContent {
+        ActivityLifeCycleTheme {
+            // A surface container using the 'background' color from the theme
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Greeting("Android")
+            }
+        }
+    }
+
+}
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hallo $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ActivityLifeCycleTheme {
+        Greeting("Android")
+    }
+}
